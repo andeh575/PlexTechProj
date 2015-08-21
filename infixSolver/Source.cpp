13c39d47ea6dbf9d9271b5b70cc8dbd96c37f458
Andrew Graham <andrew.t.graham@live.com>
2015-08-21 08:55:33 -0700

    Converted to class design. Still very procedural.

--- infixSolver/Source.cpp
+++ infixSolver/Source.cpp
@@ -15,25 +15,60 @@
 #include "ExpressionTree.h"
 using namespace std;
 
+class Node {
+	private:
+		char val;
+		Node* left;
+		Node* right;
+	public:
+		Node(char data) {
+			val = data;
+			left = NULL;
+			right = NULL;
+		}
+
+		char value() {
+			return val;
+		}
+		
+		Node* getLeft() {
+			return left;
+		}
+
+		void setLeft(Node* data) {
+			left = data;
+		}
+
+		Node* getRight() {
+			return right;
+		}
+
+		void setRight(Node* data) {
+			right = data;
+		}
+		 
+}; // End class Node
+
+/*
 struct node {
 	char val;
 	node* left;
 	node* right;
-};
+};*/
 
-string getInfix(void);				// Helper function to get initial infix expression
-void pressToContinue();				// Helper function for console development
+string getInfix(void);		// Helper function to get initial infix expression
+void pressToContinue();		// Helper function for console development
 
-bool isOperator(char m);			// Helper function for identifying mathematical operators	
-int priority(char m);				// Helper function to specify order of operations
-struct node* createNode(char m);	// Helper function to create a new node
-void insertOp(stack<node*>& operators, stack<node*>& treeNodes);	// Build a tree node with an operator and two children
-struct node* parseExpression(string m, struct node* root);			// Parse the input stack for insertion into the expression tree
-int evaluate(struct node* node);									// Recursively calculate the value of the expression tree
+bool isOperator(char m);	// Helper function for identifying mathematical operators	
+int priority(char m);		// Helper function to specify order of operations
+Node* createNode(char m);	// Helper function to create a new node
+void insertOp(stack<Node*>& operators, stack<Node*>& treeNodes);	// Build a tree node with an operator and two children
+Node* parseExpression(string m, Node* root);			// Parse the input stack for insertion into the expression tree
+int evaluate(Node* node);									// Recursively calculate the value of the expression tree
 
-void preOrder(struct node* node);	// Test function: Preorder Traversal - Recursively defined
-void inOrder(struct node* node);	// Test function: Inorder Traversal - Recursively defined
-void postOrder(struct node* node);	// Test Function: Postorder Traversal - Recursively defined
+void preOrder(Node* node);	// Test function: Preorder Traversal - Recursively defined
+void inOrder(Node* node);	// Test function: Inorder Traversal - Recursively defined
+void postOrder(Node* node);	// Test Function: Postorder Traversal - Recursively defined
 
 // Helper function to get initial expression - should be infix notation (We don't currently test)
 string getInfix(void) {
@@ -72,12 +107,12 @@ int priority(char val) {
 }
 
 // Helper function to create a new node*
-struct node* createNode(char data) {
-	struct node* subNode = new node;
+Node* createNode(char data) {
+	Node* subNode = new Node(data);
 
-	subNode->val = data;
-	subNode->left = NULL;
-	subNode->right = NULL;
+	//subNode->val = data;
+	//subNode->left = NULL;
+	//subNode->right = NULL;
 
 	return subNode;
 }
@@ -88,14 +123,14 @@ struct node* createNode(char data) {
 /		- Two Operators (ie: + and *) - Need to have subtrees of their own
 /		- An Operator and an Operand (ie: 1 and +) - Operand is a leaf and Operators have subtrees of their own
 */
-void insertOp(stack<node*>& operators, stack<node*>& treeNodes) {
-	node* opNode = operators.top();	// Take the first operator from the operators stack
+void insertOp(stack<Node*>& operators, stack<Node*>& treeNodes) {
+	Node* opNode = operators.top();	// Take the first operator from the operators stack
 	operators.pop();
 
 	// Take first two items off of the treeNodes stack and make them the opNode's children - left precedence
-	opNode->left = treeNodes.top();
+	opNode->setLeft(treeNodes.top());
 	treeNodes.pop();
-	opNode->right = treeNodes.top();
+	opNode->setRight(treeNodes.top());
 	treeNodes.pop();
 
 	// Push the newly created tree on top of the treeNodes stack
@@ -103,11 +138,11 @@ void insertOp(stack<node*>& operators, stack<node*>& treeNodes) {
 }
 
 // Parse the infix string for insertion into an expression tree
-struct node* parseExpression(string infix, struct node* root) {
+Node* parseExpression(string infix, Node* root) {
 	char temp;				
 	stack<char> input;		// Stack for input string
-	stack<node*> operators;	// Stack for handling operators in the input stack
-	stack<node*> treeNodes;	// Stack for arranging nodes for tree assembly
+	stack<Node*> operators;	// Stack for handling operators in the input stack
+	stack<Node*> treeNodes;	// Stack for arranging nodes for tree assembly
 
 	// Let's get the initial input in infix notation and break it down onto the input stack
 	for (int i = 0; i < infix.length(); i++)
@@ -132,11 +167,11 @@ struct node* parseExpression(string infix, struct node* root) {
 					operators.push(createNode(temp));	// Turn temp into a node and push it onto the operators stack
 					control = true;
 				}
-				else if (operators.top()->val == ')') {	// Is the top of the stack a closing parenthesis ')'? 
+				else if (operators.top()->value() == ')') {	// Is the top of the stack a closing parenthesis ')'? 
 					operators.push(createNode(temp));	// Turn temp into a node and and push it onto the operator stack
 					control = true;
 				}
-				else if (priority(operators.top()->val) <= priority(temp)) {	// Is the top of the stack the same or lower priority than this item? 
+				else if (priority(operators.top()->value()) <= priority(temp)) {	// Is the top of the stack the same or lower priority than this item? 
 					operators.push(createNode(temp));	// Turn temp into a node and push it onto the operator stack
 					control = true;
 				}
@@ -147,7 +182,7 @@ struct node* parseExpression(string infix, struct node* root) {
 		}
 
 		if (temp == '(') {	// Is it an opening parenthesis '('? 
-			while (operators.top()->val != ')') {	// Pop operators from the operator stack until we find a closing parenthesis ')'
+			while (operators.top()->value() != ')') {	// Pop operators from the operator stack until we find a closing parenthesis ')'
 				insertOp(operators, treeNodes);	// Insert operators into the tree
 			}
 
@@ -166,63 +201,63 @@ struct node* parseExpression(string infix, struct node* root) {
 }
 
 // Recursively calculate the value of the expression tree
-int evaluate(struct node* node) {
+int evaluate(Node* node) {
 	int x, y, z;
 
 	// The node contains an operator
-	if (node->val == '+' || node->val == '*') {
-		x = evaluate(node->left);
-		y = evaluate(node->right);
+	if (node->value() == '+' || node->value() == '*') {
+		x = evaluate(node->getLeft());
+		y = evaluate(node->getRight());
 
-		if (node->val == '+')
+		if (node->value() == '+')
 			z = x + y;
-		else if (node->val == '*')
+		else if (node->value() == '*')
 			z = x * y;
 		
 		return z;
 	}
 	// The node contains an operand
 	else
-		return(node->val - '0'); 
+		return(node->value() - '0'); 
 }
 
 /*	Test Function: Preorder Traversal - Recursively Defined - Should print prefix notation
 /	Reminder -- Preorder traversal: Root -> Left Child -> Right Child
 */
-void preOrder(struct node* node) {
+void preOrder(Node* node) {
 	if (node != NULL) {
-		cout << node->val << " ";
-		preOrder(node->left);
-		preOrder(node->right);
+		cout << node->value() << " ";
+		preOrder(node->getLeft());
+		preOrder(node->getRight());
 	}
 }
 
 /*	Test Function: Inorder Traversal - Recursively Defined - Should print infix notation
 /	Reminder -- Inorder traversal: Left Child -> Root -> Right Child
 */
-void inOrder(struct node* node) {
+void inOrder(Node* node) {
 	if (node != NULL) {
-		inOrder(node->left);
-		cout << node->val << " ";
-		inOrder(node->right);
+		inOrder(node->getLeft());
+		cout << node->value() << " ";
+		inOrder(node->getRight());
 	}
 }
 
 /*	Test Function: Postorder Traversal - Recursively Defined - Should print postfix notation
 /	Reminder -- Postorder traversal: Left Child -> Right Child -> Root
 */
-void postOrder(struct node* node) {
+void postOrder(Node* node) {
 	if (node != NULL) {
-		postOrder(node->left);
-		postOrder(node->right);
-		cout << node->val << " ";
+		postOrder(node->getLeft());
+		postOrder(node->getRight());
+		cout << node->value() << " ";
 	}
 }
 
 int main(void) {
 	string infix;	// Input string
 	int solution;	// Solution to the expression tree
-	struct node* root = new node;	// The root of the expression tree
+	Node* root = new Node(NULL);	// The root of the expression tree
 
 	cout << "Welcome to a Simple Infix Equation Solver" << endl;
 
@@ -231,7 +266,6 @@ int main(void) {
 	root = parseExpression(infix, root);
 
 	// Test for proper tree structure and printing
-	/*
 	cout << endl << "Preorder Traversal: ";
 	preOrder(root);
 
@@ -240,7 +274,6 @@ int main(void) {
 
 	cout << endl << "Postorder Traversal: ";
 	postOrder(root);
-	*/
 
 	// Solve the equation
 	solution = evaluate(root);
